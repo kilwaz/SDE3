5533465ae285e55608fce683dd739757493ce95a
alex <alexdi@gmail.com>
2014-09-23 16:32:01 +0100

    Moved source editor HTML to separate HTML file.

--- src/application/gui/SourceTextArea.java
+++ src/application/gui/SourceTextArea.java
@@ -9,7 +9,11 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import netscape.javascript.JSObject;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SourceTextArea extends VBox {
     private SourceNode sourceNode;
@@ -23,34 +27,20 @@ public class SourceTextArea extends VBox {
         webEngine = browser.getEngine();
 
         URL editorURL = getClass().getResource("/ace-editor/src/ace.js");
-        String content = "<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <style type=\"text/css\" media=\"screen\">\n" +
-                "        #editor {\n" +
-                "            position: absolute;\n" +
-                "            top: 0;\n" +
-                "            right: 0;\n" +
-                "            bottom: 0;\n" +
-                "            left: 0;\n" +
-                "        }\n" +
-                "    </style>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "\n" +
-                "<div id=\"editor\">" + sourceNode.getSource().getSource() + "</div>\n" +
-                "\n" +
-                "<script src=\"" + editorURL.toExternalForm() + "\" type=\"text/javascript\" charset=\"utf-8\"></script>\n" +
-                "<script>\n" +
-                "    var editor = ace.edit(\"editor\");\n" +
-                "    editor.setTheme(\"ace/theme/tomorrow\");\n" +
-                "    editor.getSession().setMode(\"ace/mode/java\");\n" +
-                "    editor.on('change', function() {\n" +
-                "        java.test(editor.getValue());\n" +
-                "    });\n" +
-                "</script>\n" +
-                "</body>\n" +
-                "</html>";
+        URL sourceEditorURL = getClass().getResource("/sourceEditor.html");
+
+        String content = "";
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(sourceEditorURL.toExternalForm().replaceFirst("file:/", "")));
+            content = new String(encoded, "UTF8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        content = content.replace("[[source]]", sourceNode.getSource().getSource());
+        content = content.replace("[[aceSource]]", editorURL.toExternalForm());
 
         webEngine.loadContent(content);
         browser.getEngine().getLoadWorker().stateProperty().addListener(new ChangeListener() {
