5f00267e0066b9a4db8cca6825ebfe49226eb953
daniel-jolly <daniel.jolly@spiralinks.com>
2014-10-17 14:13:47 +0100

    TEST - Can git be updated by non-user

--- src/application/Main.java
+++ src/application/Main.java
@@ -44,6 +44,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        //TEST - Can git be updated by non-user
         this.splashStage = stage;
         showSplash();
 
