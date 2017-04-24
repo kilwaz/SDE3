a3672b824ca367361f35f5aea7e699e1da346710
kilwaz <alexdi@gmail.com>
2017-04-24 10:51:28 +0100

    Added URL to output message of http connection retry.

--- src/main/java/sde/application/net/proxy/HttpClientRetryHandler.java
+++ src/main/java/sde/application/net/proxy/HttpClientRetryHandler.java
@@ -1,8 +1,10 @@
 package sde.application.net.proxy;
 
-import sde.application.error.Error;
+import org.apache.http.HttpHost;
 import org.apache.http.client.HttpRequestRetryHandler;
 import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpCoreContext;
+import sde.application.error.Error;
 
 import java.io.IOException;
 
@@ -19,11 +21,18 @@ public class HttpClientRetryHandler implements HttpRequestRetryHandler {
             Error.HTTP_CLIENT_RETRY_MAX_ATTEMPTS_REACHED.record().create();
             return false;
         }
+
+        HttpHost httpHost = (HttpHost) httpContext.getAttribute(HttpCoreContext.HTTP_TARGET_HOST);
+        String hostName = "Unknown Hostname";
+        if (httpHost != null) {
+            hostName = httpHost.getHostName();
+        }
+
         if (exception instanceof org.apache.http.NoHttpResponseException) {
-            Error.HTTP_CLIENT_RETRY_CONNECTION_ATTEMPT.record().additionalInformation("NoHttpResponse - Attempt number " + executionCount).create();
+            Error.HTTP_CLIENT_RETRY_CONNECTION_ATTEMPT.record().additionalInformation("NoHttpResponse - Attempt number " + executionCount).additionalInformation("Host: " + hostName).create();
             return true;
         } else {
-            Error.HTTP_CLIENT_RETRY_CONNECTION_ATTEMPT.record().additionalInformation("Attempt number " + executionCount).create();
+            Error.HTTP_CLIENT_RETRY_CONNECTION_ATTEMPT.record().additionalInformation("Attempt number " + executionCount).additionalInformation("Host: " + hostName).create();
             return true;
         }
     }
